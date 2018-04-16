f27bfaa7479985937b941809341fbf666f1bbd62
awongh <akira@awongh.com>
2018-04-16 19:59:18 +0800

    working CR app

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,50 @@
+class SongsController < ActionController::Base
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update(article_params)
+      redirect_to @song
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def song_params
+      params.require(:song).permit(:title)
+    end
+end
